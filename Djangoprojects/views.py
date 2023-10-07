53673b96b042597b20cb20c09117048a41808aa1
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-10-07 06:19:57 +0545

    continuous learning

--- Djangoprojects/views.py
+++ Djangoprojects/views.py
@@ -1,8 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def homePage(request):      
-    return render(request, "index.html", {"course": "Django"})
+def homePage(request):  
+    data = {
+        'title': 'Home',
+        'bodyData': 'This is my first Django Project',
+        'class': ['first', 'second', 'third'],
+        'userDetails': [
+            {'name': 'Rahul', 'age': 20},
+            {'name': 'Rohit', 'age': 21},
+            {'name': 'Raj', 'age': 22}, 
+        ],
+        'numbers' : [1,2,3,4,5,6,7,8,9,10],
+    }    
+    return render(request, "index.html", data)
 
 
 def aboutUs(request):
