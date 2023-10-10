b0b464d5a4053fb5933115c583c93e74ba5f0143
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-10-10 10:44:44 +0545

    accordion form submission

--- static/script.js
+++ static/script.js
@@ -1,9 +1,27 @@
-// Function to submit the form and refresh the page
-        function submitForm(event) {
-            event.preventDefault(); 
-            // Here, you can add any additional form processing logic if needed.
-            // For now, let's just refresh the page:
-            location.reload();
-        }
+const myForms = document.querySelectorAll('.myForm');
+        myForms.forEach(form => {
+            form.addEventListener('submit', (event) => {
+                event.preventDefault();
 
-        // 
+                const accordion = document.getElementById('myAccordion');
+                const accordionState = [];
+                const accordionItems = accordion.querySelectorAll('.accordion-item');
+                accordionItems.forEach((item, index) => {
+                    const isExpanded = item.querySelector('.accordion-collapse').classList.contains('show');
+                    accordionState[index] = isExpanded;
+                });
+                localStorage.setItem('accordionState', JSON.stringify(accordionState));
+
+                location.reload();
+            });
+        });
+
+        const savedAccordionState = JSON.parse(localStorage.getItem('accordionState'));
+        if (savedAccordionState) {
+            const accordionItems = document.querySelectorAll('.accordion-item');
+            accordionItems.forEach((item, index) => {
+                if (savedAccordionState[index]) {
+                    item.querySelector('.accordion-collapse').classList.add('show');
+                }
+            });
+        }
