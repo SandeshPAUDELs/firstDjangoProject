19311c7083d37a269ddba8c2dd5770b44ec4ac44
Sandesh Paudel <sandeshpaudle017@gmai.com>
2023-10-10 07:09:19 +0545

    make the logic to show the page when form is submit and the page is where we have leave earlier

--- Djangoprojects/settings.py
+++ Djangoprojects/settings.py
@@ -116,13 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    # "/var/www/static/",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILE_DIRS = [
-    BASE_DIR, "static",
-    
-]
---
+++ static/script.js
@@ -0,0 +1,9 @@
+// Function to submit the form and refresh the page
+        function submitForm(event) {
+            event.preventDefault(); 
+            // Here, you can add any additional form processing logic if needed.
+            // For now, let's just refresh the page:
+            location.reload();
+        }
+
+        // 
